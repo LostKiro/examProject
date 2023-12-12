556ded27500c1f8d82672ca47589dcee4bfdc21e
Lostkiro <jiaito@yandex.ru>
2023-12-12 22:45:55 +0300

    second commit

--- projectPavelPS/models.py
+++ projectPavelPS/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class ModelPavel(models.Model):
+    name = models.CharField(max_length=10)
+    email = models.CharField(max_length=20)
+    surname = models.CharField(max_length=10)
+    password = models.CharField(max_length=10)
 
+
+class ModelPavelImg(models.Model):
+    name = models.CharField(max_length=10)
+    img = models.ImageField(upload_to='imgs')
+
+class ModelRegForm(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    avatar = models.ImageField(upload_to='imgs')
--- projectPavelPS/views.py
+++ projectPavelPS/views.py
@@ -2,16 +2,37 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from projectPavelPS.models import ModelRegForm
+
 
 def index(recuest):
-    return render(recuest,'index.html')
+    return render(recuest, 'index.html')
 
-def author(recuest):
-    return render(recuest,'author.html')
 
 @csrf_exempt
-def login(recuest):
-    if recuest.method == 'POST':
-        return HttpResponse('Регистрация прошла успешно')
-    return render(recuest,'login.html')
+def login(request):
+    reg = ModelRegForm()
+    if request.method == 'POST':
+        data = ModelRegForm.objects.all()
+        for i in data:
+            if request.POST['email'] == i.email:
+                return render(request, 'index.html', {"err": f'Email: {i.email} занят другим пользователем'})
+        reg.email = request.POST['email']
+        reg.password = request.POST['password']
+        reg.login = request.POST['login']
+        reg.avatar = request.POST['avatar']
+        reg.save()
+        return HttpResponse(f'Пользователь с Email: {reg.email} успешно зарегистрирован!')
+    return render(request, 'login.html')
 
+
+@csrf_exempt
+def author(request):
+    if request.method == 'POST':
+        data = ModelRegForm.objects.all()
+        for i in data:
+            if request.POST['email'] == i.email and request.POST['password'] == i.password:
+                return HttpResponse('авторизация прошла успешно')
+            else:
+                return render(request, 'index.html', {'err': 'авторизация не пройдена'})
+    return render(request, 'author.html')
